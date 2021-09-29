b3b02ee927389ec0ae02de45812d147e5fd7dbe2
Yauheni Shatau <jack13only@gmail.com>
2021-09-29 13:53:24 +0300

    feat: add vinegret solution

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -19,13 +19,89 @@ import { NotImplementedError } from '../extensions/index.js';
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
  * 
  */
-export default class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+ export default class VigenereCipheringMachine {
+
+  constructor(reverse = true) {
+    this.reverse = reverse;
+    this.alf = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  position(a, b) {
+    // console.log(a, b);
+    return a + b > 25 ? a + b - 26 : a + b
+  }
+
+  encrypt(str, key) {
+    if (str === undefined || key === undefined) throw new Error(`Incorrect arguments!`)
+
+
+    let kF = 0;
+    let keyFull = ''
+    let output = ''
+
+    for (let i = 0; i < str.length; i++) {
+      if (kF === key.length) kF = 0
+
+      keyFull += key[kF]
+      kF++
+    }
+
+    // console.log(keyFull);
+
+    str = str.toUpperCase()
+    keyFull = keyFull.toUpperCase()
+
+
+    let j = 0
+
+    for (let i = 0; i < str.length; i++) {
+      if (this.alf.indexOf(str[i]) === -1) {
+        output += str[i]
+      } else {
+        // console.log(this.position(alf.indexOf(str[i]), alf.indexOf(keyFull[j])));
+        output += this.alf[this.position(this.alf.indexOf(str[i]), this.alf.indexOf(keyFull[j]))];
+        j++
+      }
+    }
+   
+    return this.reverse ? output : output.split('').reverse().join('')
+    
+  }
+
+  decrypt(str, key) {
+    if (str === undefined || key === undefined) throw new Error(`Incorrect arguments!`)
+
+    let kF = 0;
+    let keyFull = ''
+    let output = ''
+
+    for (let i = 0; i < str.length; i++) {
+      if (kF === key.length) kF = 0
+
+      keyFull += key[kF]
+      kF++
+    }
+
+    str = str.toUpperCase()
+    keyFull = keyFull.toUpperCase()
+
+
+    let j = 0
+
+    for (let i = 0; i < str.length; i++) {
+      if (this.alf.indexOf(str[i]) === -1) {
+        output += str[i]
+      } else {
+        output += this.alf[this.position(this.alf.indexOf(str[i]) - this.alf.indexOf(keyFull[j]), 26)];
+        // console.log(this.position(this.alf.indexOf(str[i]) - this.alf.indexOf(keyFull[j]), 26));
+        j++
+      }
+    }
+    
+    return this.reverse ? output : output.split('').reverse().join('')
+
   }
 }
+
+
